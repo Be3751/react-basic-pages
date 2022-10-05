41a7a1393ebd6fe3f68cb883c63c6021c75dfada
薗部匡矢 <msonobe@msonobe.local>
2022-10-05 23:47:04 +0900

    implemented text-input forms

--- src/components/TextFieldControlledExample.tsx
+++ src/components/TextFieldControlledExample.tsx
@@ -6,38 +6,88 @@ const textFieldStyles: Partial<ITextFieldStyles> = { fieldGroup: { width: 300 } 
 const narrowTextFieldStyles: Partial<ITextFieldStyles> = { fieldGroup: { width: 100 } };
 const stackTokens = { childrenGap: 15 };
 
-export const TextFieldControlledExample: React.FunctionComponent = () => {
-  const [firstTextFieldValue, setFirstTextFieldValue] = React.useState('');
-  const [secondTextFieldValue, setSecondTextFieldValue] = React.useState('');
-  const onChangeFirstTextFieldValue = React.useCallback(
+export const TextFieldControlledExample: React.FunctionComponent<{ 
+    setName: React.Dispatch<React.SetStateAction<string>>, 
+    setFurigana: React.Dispatch<React.SetStateAction<string>>,
+    setMail: React.Dispatch<React.SetStateAction<string>>,
+    setMailCheck: React.Dispatch<React.SetStateAction<string>>
+  }> = ({
+    setName, setFurigana, setMail, setMailCheck
+  }) => {
+  const onChangeName = React.useCallback(
     (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-      setFirstTextFieldValue(newValue || '');
+      setName(newValue || '');
     },
-    [],
+    [setName]
   );
-  const onChangeSecondTextFieldValue = React.useCallback(
+  const onChangeFurigana = React.useCallback(
     (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-      if (!newValue || newValue.length <= 5) {
-        setSecondTextFieldValue(newValue || '');
-      }
+      setFurigana(newValue || '');
     },
-    [],
+    [setFurigana]
+  );
+  const onChangeMail = React.useCallback(
+    (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+      setMail(newValue || '');
+    },
+    [setMail]
+  );
+  const onChangeMailCheck = React.useCallback(
+    (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+      setMailCheck(newValue || '');
+    },
+    [setMailCheck]
   );
 
   return (
     <Stack tokens={stackTokens}>
       <TextField
-        label="Basic controlled TextField"
-        value={firstTextFieldValue}
-        onChange={onChangeFirstTextFieldValue}
+        label="氏名"
+        onChange={onChangeName}
         styles={textFieldStyles}
       />
+
       <TextField
-        label="Controlled TextField limiting length of value to 5"
-        value={secondTextFieldValue}
+        label="ふりがな"
+        onChange={onChangeFurigana}
+        styles={textFieldStyles}
+      />
+
+      <TextField
+        label="メールアドレス"
+        onChange={onChangeMail}
+        styles={textFieldStyles}
+      />
+
+      <TextField
+        label="メールアドレス（確認）"
+        onChange={onChangeMailCheck}
+        styles={textFieldStyles}
+      />
+
+      {/* <TextField
+        label="電話番号"
         onChange={onChangeSecondTextFieldValue}
         styles={narrowTextFieldStyles}
       />
+
+      <TextField
+        label="郵便番号"
+        onChange={onChangeSecondTextFieldValue}
+        styles={narrowTextFieldStyles}
+      />
+
+      <TextField
+        label="住所"
+        onChange={onChangeSecondTextFieldValue}
+        styles={narrowTextFieldStyles}
+      />
+
+      <TextField
+        label="生年月日"
+        onChange={onChangeSecondTextFieldValue}
+        styles={narrowTextFieldStyles}
+      /> */}
     </Stack>
   );
 };
